1f906718f432ace5538b1b9a3795d7ccf5aa36e0
Pablo López-García <plopezgarcia@gmail.com>
2018-06-18 22:18:08 +0200

    filtered measures, tried new query

--- python/run.sh
+++ python/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python query.py > test/all.2017.trec_eval
-./trec_eval test/all.2017.qrels test/all.2017.trec_eval | grep 'Rprec'
+./trec_eval -m ndcg -m Rprec -m P test/all.2017.qrels test/all.2017.trec_eval | grep 'Rprec\|P_10\s\|ndcg'
