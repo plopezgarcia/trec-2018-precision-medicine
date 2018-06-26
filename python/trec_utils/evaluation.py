cd071546b3f26357e8bf249aab9eba11b8a003f9
Pablo <plopezgarcia@gmail.com>
2018-06-26 15:28:55 +0200

    fixed names

--- python/trec_utils/evaluation.py
+++ python/trec_utils/evaluation.py
@@ -3,11 +3,11 @@ import pandas
 import json
 from trec_utils import utils
 
-def evaluate(qrels, run, aggregated_measures={'recall_1000':'','ndcg':'', 'Rprec':'', 'P_10':''}):
+def evaluate(qrels_df, run_df, aggregated_measures={'recall_1000':'','ndcg':'', 'Rprec':'', 'P_10':''}):
     MEASURES_AGGREGATED = aggregated_measures
 
-    evaluator = pytrec_eval.RelevanceEvaluator(utils.qrels_to_pytrec_eval(qrels), utils.pytrec_eval.supported_measures)
-    results = evaluator.evaluate(utils.run_to_pytrec_eval(run))
+    evaluator = pytrec_eval.RelevanceEvaluator(utils.qrels_to_pytrec_eval(qrels_df), utils.pytrec_eval.supported_measures)
+    results = evaluator.evaluate(utils.run_to_pytrec_eval(run_df))
 
     for measure in MEASURES_AGGREGATED.keys():
         measure_all = pytrec_eval.compute_aggregated_measure(measure, [MEASURES_AGGREGATED[measure] for MEASURES_AGGREGATED in results.values()])
--- python/trec_utils/running.py
+++ python/trec_utils/running.py
@@ -2,14 +2,14 @@ import pandas
 import json
 import requests
 
-from trec_utils import utils
+from trec_utils import utils, evaluation
 
 config = utils.load_config()
 
 URL = config['ELASTIC'] + config['ABSTRACTS'] + '/_search'
 HEADERS = {'Content-type': 'application/json'}
 
-default_params = {
+default_run_params = {
     'query_template':'baseline.json',
     'disease_tie_breaker':0.5,
     'disease_boost':1.5,
@@ -17,29 +17,29 @@ default_params = {
     'gene_boost':1
 }
 
-def get_default_params():
-    return(default_params)
+def get_default_run_params():
+    return(default_run_params)
 
-def run(topics_df, params = default_params):
+def run(topics_df, run_params = default_run_params):
 
     run_id = "FIXME"
     run_tuples_list = []
 
-    print('RUN:', run_id, "TOPICS:", len(topics_df), params)
+    print('RUN:', run_id, "TOPICS:", len(topics_df), run_params)
 
     for index, topic_row in topics_df.iterrows():
 
         #print("TOPIC:", topic_row['topic'])
         # Fill template with query
-        with open('./query-templates/' + params['query_template'], 'r') as template_file:
+        with open('./query-templates/' + run_params['query_template'], 'r') as template_file:
           query = template_file.read()
           query = query.replace('{{disease}}', topic_row['disease'])
           query = query.replace('{{gene}}', topic_row['gene'])
 
-          query = query.replace('{{disease_tie_breaker}}', str(params['disease_tie_breaker']))
-          query = query.replace('{{disease_boost}}', str(params['disease_tie_breaker']))
-          query = query.replace('{{gene_tie_breaker}}', str(params['gene_tie_breaker']))
-          query = query.replace('{{gene_boost}}', str(params['gene_boost']))
+          query = query.replace('{{disease_tie_breaker}}', str(run_params['disease_tie_breaker']))
+          query = query.replace('{{disease_boost}}', str(run_params['disease_tie_breaker']))
+          query = query.replace('{{gene_tie_breaker}}', str(run_params['gene_tie_breaker']))
+          query = query.replace('{{gene_boost}}', str(run_params['gene_boost']))
 
         response = requests.post(URL, data=query, headers=HEADERS)
 
@@ -52,7 +52,7 @@ def run(topics_df, params = default_params):
 
     results = pandas.DataFrame(columns=['TOPIC_NO','Q0','ID','RANK','SCORE','RUN_NAME', 'TITLE'], data=run_tuples_list)
 
-    return(results, params)
+    return(results, run_params)
 
 
 
@@ -64,7 +64,7 @@ default_params_grid = {
     'gene_boost':[1,2,5]
 }
 
-def experiment(topics_df, qrels, params_grid=default_params_grid):
+def experiment(topics_df, qrels_df, params_grid=default_params_grid):
     print(params_grid)
     for qt in params_grid['query_template']:
         for dtb in params_grid['disease_tie_breaker']:
@@ -78,6 +78,6 @@ def experiment(topics_df, qrels, params_grid=default_params_grid):
                             'gene_tie_breaker':str(gtb),
                             'gene_boost':str(gb)
                         }
-                        run_df = run(topics_df, params)
-                        results, aggregated = evaluate(qrels, run_df)
+                        run_df, run_params_df = run(topics_df, params)
+                        results, aggregated = evaluation.evaluate(qrels_df, run_df)
                         print(aggregated)
